c096583ca6993430814f5886734276b4ac33fb42
AlexGP <alexgp7191@gmail.com>
2024-12-10 11:18:14 -0400

    Url Fix

--- src/ENV/env-variable.ts
+++ src/ENV/env-variable.ts
@@ -1,3 +1,3 @@
-export var apiURL = "http://172.16.101.113:8080/api"
-//export var apiURL = "https://localhost:7217/api"
+//export var apiURL = "http://172.16.101.113:8080/api"
+export var apiURL = "http://localhost:8080/api"
 // http://localhost:8080/api/
